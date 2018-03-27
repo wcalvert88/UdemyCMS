b86157d1339910ab6794c80a4e21a800a1b304c7
wcalvert88 <wcalvert88@gmail.com>
2018-03-27 07:30:26 -0400

    made switch statement to dynamically build pages

--- admin/posts.php
+++ admin/posts.php
@@ -20,47 +20,42 @@ include "includes/adminHeader.php";
                             <small>Author</small>
                         </h1>
 
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Author</th>
-                                    <th>Title</th>
-                                    <th>Category</th>
-                                    <th>Status</th>
-                                    <th>Image</th>
-                                    <th>Tags</th>
-                                    <th>Comments</th>
-                                    <th>Date</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php 
-                                $query = "SELECT * FROM posts";
-                                $selectPosts = mysqli_query($connection, $query);
-                                while($row = mysqli_fetch_assoc($selectPosts)) {
-                                    $postId = $row['post_id'];
-                                    $postAuthor = $row['post_author'];
-                                    $postTitle = $row['post_title'];
-                                    $postCategoryId = $row['post_category_id'];
-                                    $postStatus = $row['post_status'];
-                                    $postImage = $row['post_image'];
-                                    $postTags = $row['post_tags'];
-                                    $postCommentCount = $row['post_comment_count'];
-                                    $postDate = $row['post_date'];
-                                    echo "<tr>";
-                                    echo "<td>{$postId}</td>";
-                                    echo "<td>{$postAuthor}</td>";
-                                    echo "<td>{$postTitle}</td>";
-                                    echo "<td>{$postCategoryId}</td>";
-                                    echo "<td>{$postStatus}</td>";
-                                    echo "<td><img width='100' src='../images/{$postImage}' alt='image'</td>";
-                                    echo "<td>{$postTags}</td>";
-                                    echo "<td>{$postCommentCount}</td>";
-                                    echo "<td>{$postDate}</td>";
-                                    echo "</tr>";
-                                }
-                                ?>
+                        <?php 
+                        
+                        if (isset($_GET['source'])) {
+
+                            $source = $_GET['source'];
+
+                        } else {
+                            $source = '';
+                        }
+                        
+                        switch($source) {
+                            case "34";
+                                echo "34";
+                                break;
+                            case "100";
+                                echo "100";
+                                break;
+                            case "200";
+                                echo "200";
+                                break;
+                            default;
+                                include "includes/viewAllPosts.php";
+                                break;
+
+                        }
+                        
+                        
+                        
+                        
+                        
+                        ?>
+
+
+
+
+
                                     <td>10</td>
                                     <td>Bootstrap framework</td>
                                     <td>Edwin Diaz</td>
