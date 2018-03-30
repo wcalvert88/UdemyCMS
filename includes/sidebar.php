ab6f696f1e6bf5d28d5df1fc34782fabbbffe846
wcalvert88 <wcalvert88@gmail.com>
2018-03-30 12:15:14 -0400

    added login form to site sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -9,13 +9,32 @@
             <span class="input-group-btn">
                 <button name="submit" class="btn btn-default" type="submit">
                     <span class="glyphicon glyphicon-search"></span>
-            </button>
+                </button>
             </span>
         </div>
         </form><!-- search form -->
         <!-- /.input-group -->
     </div>
 
+    <!-- Login -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+        <div class="form-group">
+            <input name="username" type="text" class="form-control" placeholder="Enter Username">
+        </div>
+
+        <div class="input-group">
+            <input name="password" type="password" class="form-control" placeholder="Enter Password">
+            <span class="input-group-btn">
+                <button class="btn btn-primary" name="login" type="submit">Submit
+                </button>
+            </span>
+
+        </div>
+        </form><!-- search form -->
+        <!-- /.input-group -->
+    </div>
 
 
 
@@ -41,9 +60,6 @@
 
             </ul>
         </div>
-
-
-       
     </div>
     <!-- /.row -->
 </div>
