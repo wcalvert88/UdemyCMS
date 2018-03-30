6151b0e2778d6c9278d4cecab2278dadc414a5f9
wcalvert88 <wcalvert88@gmail.com>
2018-03-30 12:40:26 -0400

    protected from sql injection and search db for username

--- includes/login.php
+++ includes/login.php
@@ -2,9 +2,18 @@
 <?php 
 
 if (isset($_POST['login'])) {
-    echo $_POST['username'];
-    echo $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 }
+$username = mysqli_real_escape_string($connection, $username);
+$password = mysqli_real_escape_string($connection, $username);
 
-
+$query = "SELECT * FROM users WHERE username = '{$username}' ";
+$selectUserQuery = mysqli_query($connection, $query);
+if (!$selectUserQuery) {
+    die ("QUERY FAILED" . mysqli_error($connection));
+}
+while ($row = mysqli_fetch_array($selectUserQuery)) {
+    echo $dbId = $row['user_id'];
+}
 ?>
