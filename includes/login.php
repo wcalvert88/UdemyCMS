dd8934e51ef650e05efa93a8592163bfc770bbd0
wcalvert88 <wcalvert88@gmail.com>
2018-03-30 13:00:13 -0400

    got login function to work

--- includes/login.php
+++ includes/login.php
@@ -6,14 +6,28 @@ if (isset($_POST['login'])) {
     $password = $_POST['password'];
 }
 $username = mysqli_real_escape_string($connection, $username);
-$password = mysqli_real_escape_string($connection, $username);
-
+$password = mysqli_real_escape_string($connection, $password);
+echo $username;
+echo $password;
 $query = "SELECT * FROM users WHERE username = '{$username}' ";
 $selectUserQuery = mysqli_query($connection, $query);
 if (!$selectUserQuery) {
     die ("QUERY FAILED" . mysqli_error($connection));
 }
 while ($row = mysqli_fetch_array($selectUserQuery)) {
-    echo $dbId = $row['user_id'];
+    $dbUserId = $row['user_id'];
+    $dbUsername = $row['username'];
+    $dbUserPassword = $row['user_password'];
+    $dbUserFirstname = $row['user_firstname'];
+    $dbUserLastname = $row['user_lastname'];
+    $dbUserRole = $row['user_role'];
+}
+
+if ($username !== $dbUsername && $password !== $dbUserPassword) {
+    header("Location: ../index.php");
+} else if ($username == $dbUsername && $password == $dbUserPassword) {
+    header("Location: ../admin");
+} else {
+    header("Location: ../index.php");
 }
 ?>
