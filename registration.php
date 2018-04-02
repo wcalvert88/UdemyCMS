a502f57eb615590c41ecf9eb21ecf776fc1b4981
wcalvert88 <wcalvert88@gmail.com>
2018-04-02 18:44:50 -0400

    set variables and prevented mysql injections

--- registration.php
+++ registration.php
@@ -5,7 +5,13 @@
 
 if (isset($_POST['submit'])) {
 
-    echo "It's working";    
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $email = mysqli_real_escape_string($connection, $email);
+    $password = mysqli_real_escape_string($connection, $password);
 
 }
 
