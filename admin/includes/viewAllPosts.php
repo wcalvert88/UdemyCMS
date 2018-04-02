dc368e1584e8f160dab2d45a1f63c9fdd67b4000
wcalvert88 <wcalvert88@gmail.com>
2018-04-02 16:01:29 -0400

    add view post section to view all posts in admin

--- admin/includes/viewAllPosts.php
+++ admin/includes/viewAllPosts.php
@@ -59,6 +59,7 @@ if (isset($_POST['checkBoxArray'])){
             <th>Tags</th>
             <th>Comments</th>
             <th>Date</th>
+            <th>View Post</th>
             <th>Edit</th>
             <th>Delete</th>
         </tr>
@@ -106,6 +107,7 @@ if (isset($_POST['checkBoxArray'])){
             echo "<td>{$postTags}</td>";
             echo "<td>{$postCommentCount}</td>";
             echo "<td>{$postDate}</td>";
+            echo "<td><a href='../post.php?p_id={$postId}'>View Post</td>";
             echo "<td><a href='posts.php?source=edit_post&p_id={$postId}'>Edit</td>";
             echo "<td><a href='posts.php?delete={$postId}'>Delete</td>";
             echo "</tr>";
