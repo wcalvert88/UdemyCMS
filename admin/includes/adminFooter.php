f66efe3bc8d5e5d066fdbe107e95d60fd1ef8293
wcalvert88 <wcalvert88@gmail.com>
2018-04-02 15:41:31 -0400

    made check all boxes work using JS

--- admin/includes/adminFooter.php
+++ admin/includes/adminFooter.php
@@ -4,10 +4,10 @@
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-    <script src="js/scripts.js"></script>
+    
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
     .catch( error => {
         console.error( error );
     } );
+
+    $('#selectAllBoxes').click(function(event) {
+        if(this.checked) {
+            $('.checkBoxes').each(function(){
+                this.checked = true;
+            });
+        } else {
+            $('.checkBoxes').each(function() {
+                this.checked = false;
+            })
+        }
+    })
 });
