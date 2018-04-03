6b5a1c11d326ff85b24080a8434ea134b29f4971
wcalvert88 <wcalvert88@gmail.com>
2018-04-03 09:15:05 -0400

    added loading screen has a bug where loading screen shows afterpage loads, removing this feature after this commit

--- admin/includes/adminHeader.php
+++ admin/includes/adminHeader.php
@@ -45,6 +45,8 @@ if(!isset($_SESSION['userRole'])) {
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <link href="css/styles.css" rel="stylesheet">
+
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/1.0.0-beta.1/classic/ckeditor.js"></script>
 </head>
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -16,5 +16,10 @@ $(document).ready(function() {
                 this.checked = false;
             })
         }
-    })
+    });
+    var divBox = "<div id='load-screen'><div id='loading'></div></div>";
+    $("body").prepend(divBox);
+    $('#load-screen').delay(700).fadeOut(600, function() {
+        $(this).remove();
+    });
 });
