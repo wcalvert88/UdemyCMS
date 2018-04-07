b89efed2f915df92a7a8d996c337a5c02217e31f
wcalvert88 <wcalvert88@gmail.com>
2018-04-07 15:42:01 -0400

    started joining tables

--- admin/includes/viewAllPosts.php
+++ admin/includes/viewAllPosts.php
@@ -93,7 +93,12 @@ if (isset($_POST['checkBoxArray'])){
     </thead>
     <tbody>
         <?php 
-        $query = "SELECT * FROM posts ORDER BY post_id DESC ";
+        // $query = "SELECT * FROM posts ORDER BY post_id DESC ";
+        $query = "SELECT posts.post_id, posts.post_author, posts.post_category_id, posts.post_title, posts.post_user, posts.post_date, posts.post_image, posts.post_content, posts.post_tags, posts.post_comment_count, posts.post_status, categories.cat_id, categories.cat_title FROM posts";
+        $query .= "LEFT JOIN categories ON posts.post_category_id = categories.cat_id ";
+
+
+        
         $selectPosts = mysqli_query($connection, $query);
         while($row = mysqli_fetch_assoc($selectPosts)) {
             $postId = escape($row['post_id']);
