d18ccc60da13c2fd2cc472838eeaa4b12ba41921
wcalvert88 <wcalvert88@gmail.com>
2018-04-09 19:06:42 -0400

    got pusher installed in app

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php include "includes/db.php"; 
-include "includes/header.php"; ?>
-<?php 
+include "includes/header.php"; 
 
+require 'vendor/autoload.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = escape($_POST['username']);
     $email = escape($_POST['email']);
